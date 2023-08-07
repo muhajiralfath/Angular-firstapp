dfc281f6107ad96abb0b66a5a0283d1982a590e5
Muhammad Muhajir Al Fath <muhammadmuhajiralfath@gmail.com>
2023-08-07 16:41:38 +0700

    re set-up project

--- src/app/hello/hello.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: 'hello-world',
-  templateUrl: './hello.component.html',
-  styleUrls: ['hello.component.css']
-})
-
-export class HelloComponent { }
