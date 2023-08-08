379103141bc90d0cd0d036aca118532097e660e6
Muhammad Muhajir Al Fath <muhammadmuhajiralfath@gmail.com>
2023-08-08 14:40:06 +0700

    new page login adn add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./auth/login/login.component";
+import {RegisterComponent} from "./auth/register/register.component";
+import {ForgotPasswordComponent} from "./auth/forgot-password/forgot-password.component";
+import {HomeComponent} from "./component/home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: LoginComponent
+  },
+  {
+    path: 'register', component: RegisterComponent
+  },
+  {
+    path: 'forgot-password', component: ForgotPasswordComponent
+  },
+  {
+    path: 'home', component: HomeComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
+import { BodyComponent } from './component/body/body.component';
+import { ArticleComponent } from './shared/component/article/article.component';
+import { FooterComponent } from './shared/component/footer/footer.component';
+import { SidebarComponent } from './shared/component/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/component/header/header.component';
+import { HomeComponent } from './component/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgotPasswordComponent,
+    BodyComponent,
+    ArticleComponent,
+    FooterComponent,
+    SidebarComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
