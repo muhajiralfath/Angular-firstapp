ae7cf3ef7b3f0c6e2f744095b50a8c591cc13f1f
Muhammad Muhajir Al Fath <muhammadmuhajiralfath@gmail.com>
2023-08-11 17:10:23 +0700

    form validators

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {LoginComponent} from "./auth/login/login.component";
-import {RegisterComponent} from "./auth/register/register.component";
-import {ForgotPasswordComponent} from "./auth/forgot-password/forgot-password.component";
-import {HomeComponent} from "./component/home/home.component";
+
 
 const routes: Routes = [
-  {
-    path: '', component: LoginComponent
-  },
-  {
-    path: 'register', component: RegisterComponent
-  },
-  {
-    path: 'forgot-password', component: ForgotPasswordComponent
-  },
-  {
-    path: 'home', component: HomeComponent
-  }
-
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'title from parent';
-  name: string = "data parent awal"
 }
