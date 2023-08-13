e23dfcb9a3ce113b46e91305344cd3ee3c09a3df
Muhammad Muhajir Al Fath <muhammadmuhajiralfath@gmail.com>
2023-08-13 13:23:01 +0700

    directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,16 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { AnimeFormComponent } from './anime/anime-form/anime-form.component';
 import { AnimeListComponent } from './anime/anime-list/anime-list.component';
 
+import {
+  ForbiddenValidatorDirective
+} from './shared/utils/forbidden-name/forbidden-name.directive';
+
 @NgModule({
   declarations: [
     AppComponent,
     AnimeFormComponent,
     AnimeListComponent,
+    ForbiddenValidatorDirective,
   ],
   imports: [
     BrowserModule,
